5c10ff25f03c49433b98a4fea243f1f8e5b689c3
pushpak <pushpakithule@gmail.com>
2024-05-18 23:22:38 +0530

    first part

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,31 @@
 <template>
-  <div>Hello</div>
+  <Header />
+  <div class="container">
+    <Balance :total="total" />
+    <IncomeExpense />
+    <TransactionList :transactions="transactions" />
+    <AddTransaction />
+  </div>
 </template>
 
-<script></script>
+<script setup>
+import Balance from "./components/Balance.vue";
+import Header from "./components/Header.vue";
+import IncomeExpense from "./components/IncomeExpense.vue";
+import TransactionList from "./components/TransactionList.vue";
+import AddTransaction from "./components/AddTransaction.vue";
+
+import { ref, computed } from "vue";
+const transactions = ref([
+  { id: 1, text: "Flower", amount: -20 },
+  { id: 2, text: "Salary", amount: 300 },
+  { id: 3, text: "Book", amount: -10 },
+  { id: 4, text: "Camera", amount: 150 },
+]);
+
+const total = computed(() => {
+  return transactions.value.reduce((acc, transaction) => {
+    return acc + transaction.amount;
+  }, 0);
+});
+</script>
